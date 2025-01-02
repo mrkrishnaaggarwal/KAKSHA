8fe4b7cbaede7e72f9240c98f69f9d5d6b482a28
mrkrishnaaggarwal <krishnaaggarwal4777@gmail.com>
2025-01-02 12:24:55 +0530

    Made prof dashboard working again

--- Frontend/app/professor/layout.tsx
+++ Frontend/app/professor/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ProfessorNavbar from '@/app/components/professor/ProfessorNavbar'
+import ProfessorNavbar from '@/app/components/ProfessorNavbar'
 const layout = ({children} : {children: React.ReactNode}) => {
   return (
     <div className='flex h-full'>
