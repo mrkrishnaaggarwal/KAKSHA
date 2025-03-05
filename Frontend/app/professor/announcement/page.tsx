c982a64c6a1fad0aa8683faef5eaa0e1220d28b3
Tanmay-Singh3004 <tanmay30iit@gmail.com>
2025-03-05 17:23:02 +0530

    check

--- Frontend/app/professor/announcement/page.tsx
+++ Frontend/app/professor/announcement/page.tsx
@@ -1,12 +1,22 @@
+import AnnouncementBox from '@/app/components/AnnouncementBox'
 import React from 'react'
 function page() {
   return (
-    <>
-        <main className=''>
-          <h1>Hello from Classes page </h1>
-        </main>
-        
-    </>
+    <div className="flex-1 min-h-screen overflow-auto">
+      {/* <Sidebar /> */}
+      <div className='w-[100%]'>
+        {/* <Topbar /> */}
+        <div className='flex'><AnnouncementBox
+          classId="class123"
+          teacher={{
+            name: "Mrs. Johnson",
+            avatar: "/teacher-avatar.jpg"
+          }}
+          actionUrl="/api/announcements"
+        />
+        </div>
+      </div>
+    </div>
   )
 }
 
